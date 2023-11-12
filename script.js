8ef781e17f365e6fb73e36cc93a927d3ddc82078
rohan <rohan1706pandey@gmail.com>
2023-11-12 11:51:38 +0530

    "Upload" page for DermAI
    I have currently designed the basic structure of the application as that in the ppt, all the color codes, font styles etc.
    Also added the "Pick Image from Gallery" and it works. Choosing a file will display it on the screen and if there is any buffer, a "loading..." text will be displaced.
    
    The "Description" element doesn't really work properly as of now since it is supposed to start analyzing as soon as image is uploaded.
    
    TODO:
    - Add a navigation bar as similar as possible as that in the ppt.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function previewImage() {
+    var input = document.getElementById('image-input');
+    var selectedImage = document.getElementById('selected-image');
+    var customButton = document.getElementById('custom-button');
+    var loadingIndicator = document.getElementById('loading-indicator');
 
+    var file = input.files[0];
+
+    if (file) {
+      loadingIndicator.style.display = 'block'; // Show the loading indicator
+
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        // Update only the selected image with the chosen image file
+        selectedImage.src = e.target.result;
+
+        // Hide the "Choose File" button
+        customButton.style.display = 'none';
+
+        // Hide the loading indicator once the image is loaded
+        loadingIndicator.style.display = 'none';
+
+        // Display the description text
+        description.style.display = 'block';
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
