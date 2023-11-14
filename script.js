7880835f76322c2fbe2445470ea93961b4aec998
rohan <rohan1706pandey@gmail.com>
2023-11-14 11:54:30 +0530

    Update script,js

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 function previewImage() {
-var input = document.getElementById('image-input');
-var selectedImage = document.getElementById('selected-image');
-var customButton = document.getElementById('custom-button');
-var loadingIndicator = document.getElementById('loading-indicator');
-var description = document.getElementById('description');
+  var input = document.getElementById('image-input');
+  var selectedImage = document.getElementById('selected-image');
+  var customButton = document.getElementById('custom-button');
+  var loadingIndicator = document.getElementById('loading-indicator');
+  var description = document.getElementById('description');
 
-var file = input.files[0];
+  var file = input.files[0];
 
-if (file) {
-loadingIndicator.style.display = 'block';
+  if (file) {
+    loadingIndicator.style.display = 'block'; // Shows the loading indicator
+    var reader = new FileReader();
 
-var reader = new FileReader();
+    reader.onload = function (e) {
+      // Update only the selected image with the chosen image file
+      selectedImage.src = e.target.result;
+      customButton.style.display = 'none';
+      loadingIndicator.style.display = 'none';
+      description.innerText = 'Image uploaded successfully! Check Diagnosis below!';
+      description.style.display = 'block';
+    }
 
-reader.onload = function (e) {
-selectedImage.src = e.target.result;
-customButton.style.display = 'none';
-loadingIndicator.style.display = 'none';
-description.innerText = 'Image uploaded successfully! Check Diagnosis below!';
-description.style.display = 'block';
-};
-
-reader.readAsDataURL(file);
-}
-}
-
-
-  function toggleSlidingWindow() {
-    var slidingWindow = document.getElementById('sliding-window');
-    slidingWindow.style.bottom = slidingWindow.style.bottom === '-5%' ? '-100%' : '-5%';
+    reader.readAsDataURL(file);
   }
+}
+
+
+function toggleSlidingWindow() {
+  var slidingWindow = document.getElementById('sliding-window');
+  slidingWindow.style.bottom = slidingWindow.style.bottom === '-5%' ? '-100%' : '-5%';
+}
+
+function turnOffSlidingWindow() {
+  var slidingWindow = document.getElementById('sliding-window');
+  slidingWindow.style.bottom = slidingWindow.style.bottom === '-5%' ? '-100%' : '-100%';
+}
