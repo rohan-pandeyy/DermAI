9e424348af0cdb5b0c878757a858af0c37b5cb46
Pustak Pathak <74170759+PustakP@users.noreply.github.com>
2023-11-14 11:33:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 function previewImage() {
-    var input = document.getElementById('image-input');
-    var selectedImage = document.getElementById('selected-image');
-    var customButton = document.getElementById('custom-button');
-    var loadingIndicator = document.getElementById('loading-indicator');
+var input = document.getElementById('image-input');
+var selectedImage = document.getElementById('selected-image');
+var customButton = document.getElementById('custom-button');
+var loadingIndicator = document.getElementById('loading-indicator');
+var description = document.getElementById('description');
 
-    var file = input.files[0];
+var file = input.files[0];
 
-    if (file) {
-      loadingIndicator.style.display = 'block'; // Show the loading indicator
+if (file) {
+loadingIndicator.style.display = 'block';
 
-      var reader = new FileReader();
+var reader = new FileReader();
 
-      reader.onload = function(e) {
-        // Update only the selected image with the chosen image file
-        selectedImage.src = e.target.result;
+reader.onload = function (e) {
+selectedImage.src = e.target.result;
+customButton.style.display = 'none';
+loadingIndicator.style.display = 'none';
+description.innerText = 'Image uploaded successfully! Check Diagnosis below!';
+description.style.display = 'block';
+};
 
-        // Hide the "Choose File" button
-        customButton.style.display = 'none';
+reader.readAsDataURL(file);
+}
+}
 
-        // Hide the loading indicator once the image is loaded
-        loadingIndicator.style.display = 'none';
-
-        // Display the description text
-        description.style.display = 'block';
-      };
-
-      reader.readAsDataURL(file);
-    }
-  }
 
   function toggleSlidingWindow() {
     var slidingWindow = document.getElementById('sliding-window');
